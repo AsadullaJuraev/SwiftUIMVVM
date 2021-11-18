3af794065655a937258903bdd534f685dca2f5b1
Asadulla Juraev <tezkormedia@gmail.com>
2021-11-18 14:46:43 +0500

    Initial Commit

---
+++ SwiftUIMVVM/SwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMApp.swift
+//  SwiftUIMVVM
+//
+//  Created by Asadulla Juraev on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
